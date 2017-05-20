18de035fb1ef34d20de64e372c35deb12abd98c0
Fredi <fredifqh@gmail.com>
2017-05-20 02:41:26 -0300

    remove classes

--- proper_motion.hpp
+++ proper_motion.hpp
@@ -5,28 +5,12 @@ For example, the Time class would be declared in the file Time .h.*/
 
 #ifndef PROPERMOTIONHEADERDEF
 #define PROPERMOTIONHEADERDEF
-
-#include <bits/stdc++.h>
-
-class ProperMotion	
-{
-public:
-//private:
-	double hourRA, minuteRA, secondRA, degreDec, minuteDec, secondDec, RA, DEC, RAinDegree, DECinDegree;
-	double paralaxe, radialVelocity, time, muAlpha, muDelta;
-	double intparthourRA, intpartminuteRA, intparthourDE, intpartminuteDE;
-	std::vector<double> v = std::vector<double>(3); 
-	std::vector<double> m = std::vector<double>(3); 
-	std::vector<double> n = std::vector<double>(3);
-	std::vector<double> p = std::vector<double>(3);
-	std::vector<double> Final = std::vector<double>(6);
-//public:
-	//ProperMotion();
-	//~ProperMotion();
-	double ConvertRAToDegrees(double, double, double); // Functio to convert Right Ascention (hours, minutes and seconds) to degrees
+ 
+    double ConvertRAToDegrees(double, double, double); // Functio to convert Right Ascention (hours, minutes and seconds) to degrees
 	double ConvertDecToDegrees(double, double, double); // Functio to convert Declination (degrees, minutes and seconds) to degrees
 	std::vector<double> ConvertDegRAtoRAcoord(double, double); // Function to convert Degrees in hours, minutes and seconds
 //	std::vector<double> ConvertDegDEtoDEcoord(double); // Function to convert Degrees in Degrees, minutes and seconds
-	std::vector<double> CalculateProperMotion(double, double, double, double, double, double); // Function to get final proper motion
-};
+	std::vector<double> ProperMotion(double, double, double, double, double, double, double, double, double, double, double); // Function to get final proper motion
+
 #endif
+
